c0c0eb857b5b1c06163e36823b73bf4284a766a4
Soraya Saenna <bubblebitoey@gmail.com>
2016-04-25 18:03:06 +0700

    format print out

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String a = scan.nextLine();
-        System.out.println(a);
+        System.out.printf("%10s", a);
 
     }
 }
