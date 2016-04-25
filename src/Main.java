d25676b8a87ebee496318aebd50c941e0b033ccc
Soraya Saenna <bubblebitoey@gmail.com>
2016-04-25 18:02:45 +0700

    add scanner in program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scan = new Scanner(System.in);
+        String a = scan.nextLine();
+        System.out.println(a);
+
     }
 }
