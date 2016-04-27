bb4b5b2343b0945fb351678c8a86f1835b2ae39f
Soraya Saenna <bubblebitoey@gmail.com>
2016-04-27 20:27:11 +0700

    inherit animal class

--- src/Cat.java
+++ src/Cat.java
@@ -1,54 +1,24 @@
 /**
  * Created by kamontat on 26/4/59.
  */
-public class Cat {
+public class Cat extends Animal {
 	private String name;
 	private int age;
 
 	public Cat() {
-<<<<<<< HEAD
-		name = "your Cat";
-		age = 0;
-=======
-		name = "My Cat";
->>>>>>> 27878866459230902ae8745579f75585ac555f81
+		super();
 	}
 
 	public Cat(String name) {
-		this.name = name;
-		this.age = 0;
+		super(name);
 	}
 
 	public Cat(String name, int age) {
-<<<<<<< HEAD
-		this.age = age;
-		this.name = name;
-	}
-=======
-
-		this.name = name;
-		this.age = age;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
+		super(name, age);
 	}
 
 	@Override
 	public String toString() {
 		return "Cat{" + "name='" + name + '\'' + ", age=" + age + '}';
 	}
->>>>>>> 27878866459230902ae8745579f75585ac555f81
 }
